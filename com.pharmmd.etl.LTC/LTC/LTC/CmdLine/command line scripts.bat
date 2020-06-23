bf7a5b35226d7bf7d738de23377d57494617305d
Brian Williams <brian.williams@adherehealth.com>
2020-06-23 15:23:26 -0500

    Add files via upload

---
+++ com.pharmmd.etl.LTC/LTC/LTC/CmdLine/command line scripts.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Microsoft SQL Server\100\DTS\Binn\dtexec.exe" /f "C:\Users\brian.williams.PHARMMD\Documents\Visual Studio 2008\Projects\SSIS\DeathAndDisenrollment\DND\Generic_DDV.dtsx" /conf "SCANConfig.dtsConfig"
